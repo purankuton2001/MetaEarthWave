b4c584e86ded0f5797a6a3016d07b03fd5c98c17
purankuton2001 <purankuton2001@icloud.com>
2022-11-09 23:01:55 +0900

    modify error

--- src/hooks/useLanguage.ts
+++ src/hooks/useLanguage.ts
@@ -1,7 +1,7 @@
 import {useEffect, useState} from 'react';
 
 export const useLanguage = () => {
-  const [language, setLanguage] = useState<string>(!null);
+  const [language, setLanguage] = useState<string>(null!);
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
