c28febea48cbaab3ffe83da352efd894d7363951
purankuton2001 <purankuton2001@icloud.com>
2022-11-10 18:07:04 +0900

    modify jwt callback

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -15,9 +15,9 @@ export const authOptions = {
     async jwt({token, account}: any) {
       // Persist the OAuth access_token to the token right after signin
       if (account) {
-        token.accessToken = account.access_token;
+        token.account = account;
       }
-      return {...token, ...account};
+      return {...token};
     },
     async session({session, token, user}:any) {
       // Send properties to the client, like an access_token from a provider.
