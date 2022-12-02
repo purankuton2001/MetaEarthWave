738809b575edcba8b52711094e8ef3823688c16e
purankuton2001 <purankuton2001@icloud.com>
2022-12-02 16:18:58 +0900

    add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,14 @@ function MyApp({Component, pageProps: {session, ...pageProps}}: AppProps) {
       <Head>
         <title>MetaEarthWave</title>
 
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+
         <meta
           name="viewport"
           content="initial-scale=1.0, width=device-width" />
@@ -34,6 +42,7 @@ function MyApp({Component, pageProps: {session, ...pageProps}}: AppProps) {
         <meta
           property="og:image"
           content="https://www.meta-earth-wave.art/assets/images/MetaEarthWave.png" />
+
       </Head>
       <ChakraProvider>
         <Component {...pageProps} />
