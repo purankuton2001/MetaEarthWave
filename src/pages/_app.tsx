f969ed20c06830776b4502ecc8f96bd25ea12e41
purankuton2001 <purankuton2001@icloud.com>
2022-12-02 16:27:00 +0900

    modify favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,7 +14,9 @@ function MyApp({Component, pageProps: {session, ...pageProps}}: AppProps) {
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
         <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
         <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
 
