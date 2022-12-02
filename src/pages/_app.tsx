64a086debc199b7a93e60a84fc6d648a809fbc60
purankuton2001 <purankuton2001@icloud.com>
2022-12-02 15:07:35 +0900

    modify meta tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,18 +10,24 @@ function MyApp({Component, pageProps: {session, ...pageProps}}: AppProps) {
       <Head>
         <title>MetaEarthWave</title>
         <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content="@plankton_2022" />
+        <meta name="twitter:title" content="MetaEarthWave" />
         <meta
           name="twitter:description"
           content="Make waves on the planet with your tweets" />
+        <meta
+          name="twitter:image:src"
+          content="https://www.meta-earth-wave.art/assets/images/MetaEarthWave.png" />
+
         <meta property="og:site_name" content="MetaEarthWave" />
+        <meta property="og:url" content="https://www.meta-earth-wave.art" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="MetaEarthWave" />
         <meta
           property="og:description"
           content="Make waves on the planet with your tweets" />
         <meta
           property="og:image"
-          content="https://www.meta-earth-wave.art/assets/images/MetaEarthWave.png" />
-        <meta
-          name="twitter:image:src"
           content="https://www.meta-earth-wave.art/assets/images/MetaEarthWave.png" />
         <meta
           name="viewport"
